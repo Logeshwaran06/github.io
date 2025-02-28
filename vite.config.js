ec0128a2c214e46fcec951f424eae09bcfcf9924
Logeshwaran06 <logeshmohan4080@gmail.com>
2025-02-28 21:02:36 +0530

    Update .gitignore and remove untracked files

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: "/github.io/", // Use "/<repo-name>/" if your repo is NOT named Logeshwaran06.github.io
+  base: "/", 
   plugins: [react()],
 });
