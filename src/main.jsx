64b2e309489090f8409441acd9fc2861d7fe826e
Logeshwaran06 <logeshmohan4080@gmail.com>
2025-03-02 22:58:09 +0530

    Initial commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+<<<<<<< HEAD
 import './index.css'
+=======
+// import './index.css'
+>>>>>>> 919fbc3 (Initial commit)
 import App from './App.jsx'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
