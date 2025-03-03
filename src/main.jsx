cf1ad140766fc7f01add6893a4e13d5b4fd55146
Logeshwaran06 <logeshmohan4080@gmail.com>
2025-03-03 10:05:41 +0530

    Updated package.json and main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,23 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-<<<<<<< HEAD
-import './index.css'
-=======
-// import './index.css'
->>>>>>> 919fbc3 (Initial commit)
-import App from './App.jsx'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.jsx';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 createRoot(document.getElementById('root')).render(
-    <StrictMode>
-        <App />
-        <BrowserRouter>
-            <Routes>
-                {/* <Route path='/portfolio/home' element={} /> */}
-                {/* <Route path='/home' /> */}
-
-            </Routes>
-        </BrowserRouter>
-    </StrictMode>
- 
-)
+  <StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        {/* Add your other routes here */}
+      </Routes>
+    </BrowserRouter>
+  </StrictMode>
+);
